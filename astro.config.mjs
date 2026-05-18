0bc65bddd1995f02733c9bca2bfe9b60ed99adad
Michael <michael@campbellssource.com>
2026-05-18 16:43:06 +0100

    cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'server',
+  adapter: node({ mode: 'standalone' }),
 });
--- keystatic.config.ts
+++ keystatic.config.ts
@@ -9,7 +9,9 @@ const richText = (label: string) =>
   });
 
 export default config({
-  storage: { kind: 'local' },
+  storage: process.env.NODE_ENV === 'production'
+    ? { kind: 'github', repo: 'campbellssource/meadowbrookdartingtonorg' }
+    : { kind: 'local' },
 
   singletons: {
     homepage: singleton({
